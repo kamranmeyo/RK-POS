5397679f41188813eb16eed2067908eb567e6430
Rashi <Mark 1@DESKTOP-VQA0NK0>
2023-10-09 23:23:45 -0700

    USERLIST AND PRODUCT LIST

--- RK POS/Program.cs
+++ RK POS/Program.cs
@@ -1,9 +1,9 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using RK_POS.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using MudBlazor.Services;
-
-
+using RK_POS.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,14 +19,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.MapBlazorHub();
